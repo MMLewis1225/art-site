03d587d6c00784df269ad26584c9a84e6ee77635
RoboticMind <30132912+RoboticMind@users.noreply.github.com>
2025-04-21 15:54:17 -0400

    Add Client Side Validation of Signup Reqs

--- src/templates/signup.php
+++ src/templates/signup.php
@@ -41,9 +41,9 @@
         <?php endif; ?>
         
         <form class="form-signin" method="POST" action="index.php?command=signup_handler">
-          <h1 class="h3 mb-3 fw-normal">Create an Account</h1>
-          
+          <h1 class="h3 mb-3 fw-normal">Create an Account</h1>          
           <div class="mb-3">
+            <div class="alert alert-danger" id="usernameError"></div>
             <label for="inputUsername" class="form-label">Username</label>
             <input
               type="text"
@@ -70,6 +70,7 @@
           </div>
           
           <div class="mb-3">
+            <div class="alert alert-danger" id="passwordError"></div>
             <label for="inputPassword" class="form-label">Password</label>
             <input
               type="password"
@@ -115,5 +116,110 @@
     integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
     crossorigin="anonymous"
   ></script>
+  <script
+    src="https://code.jquery.com/jquery-3.7.1.min.js"
+    integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="
+    crossorigin="anonymous"></script>
+
+    <script>
+      //ref: https://stackoverflow.com/questions/9862761/how-to-check-if-character-is-a-letter-in-javascript
+      function isLetter(char) {
+        return char.toLowerCase() != char.toUpperCase();
+      }
+
+      function isUpperCase(char){
+        return char === char.toUpperCase();
+      }
+
+      function isNumeric(char) {
+        return char.match(/[0-9]/);
+      }
+
+      $("#usernameError").hide();
+      $("#passwordError").hide();
+
+      $(".form-signin").on("submit", function(event){
+        const username = $("#inputUsername").val()
+
+        if (username.length < 3){
+          event.preventDefault(); //don't submit
+          $("#usernameError").text("Username too short. Must be at least 3 characters").show();
+          return;
+        }
+
+        if (username.length > 20){
+          event.preventDefault();
+          $("#usernameError").text("Username too long. Must be under 20 characters").show();
+          return;
+        }
+
+        for (const char of username){
+          if (!isLetter(char) && !isNumeric(char)){
+            event.preventDefault();
+            $("#usernameError").text("Username may only contain letters and numbers").show();
+            return;
+          }
+        }
+
+        //if no error in the username, hide any previous error
+        $("#usernameError").hide();
+
+        const inputPassword = $("#inputPassword").val();
+        const confirmPassword = $("#inputConfirmPassword").val();
+
+        console.log(inputPassword);
+        console.log(confirmPassword);
+
+
+        if (inputPassword !== confirmPassword){
+          event.preventDefault();
+          $("#passwordError").text("Passwords do not match").show();
+          return;
+        }
+
+        if (inputPassword.length < 8){
+          event.preventDefault();
+          $("#passwordError").text("Passwords too short. Must be at least 8 characters").show();
+          return;
+        }
+
+        let containsUppercase = false;
+        let containsLowercase = false;
+        let containsNumber = false;
+        for (const char of inputPassword){
+          if (isNumeric(char)){
+            containsNumber = true;
+          } else if (isLetter(char)){
+            if (isUpperCase(char)){
+              containsUppercase = true;
+            } else {
+              containsLowercase = true;
+            }
+          }
+        }
+
+        if (!containsNumber){
+          event.preventDefault();
+          $("#passwordError").text("Passwords must contain a number").show();
+          return;
+        }
+
+        if (!containsUppercase){
+          event.preventDefault();
+          $("#passwordError").text("Passwords must contain an uppercase letter").show();
+          return;
+        }
+
+        if (!containsLowercase){
+          event.preventDefault();
+          $("#passwordError").text("Passwords must contain a lowercase letter").show();
+          return;
+        }
+
+        //if no error in the username, hide any previous error
+        $("#passwordError").hide();
+
+      })
+    </script>
 </body>
 </html>
